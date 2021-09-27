764ee81a5416a41fc5869b2aa519f1b69cd9b0be
Hellcat44 <89856017+Hellcat44@users.noreply.github.com>
2021-09-27 23:02:32 +0600

    Add files via upload

---
+++ Projects/Config/main.php
@@ -0,0 +1,12 @@
+<?php $names = [ 
+    "main_page" => "Main Page",
+    "courses" => "Courses",
+    "a_us" => "About Us",
+    "reg" => "Registration",
+    "sign_in" => "Sign In",
+    "price" => "Prices",
+    "a_html" => "About HTML",
+    "a_css" => "About CSS",
+    "a_php" => "About PHP",
+
+    ]; ?>
